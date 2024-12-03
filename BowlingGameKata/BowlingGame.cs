3f3c7b95a93d74a2240fa819ce79a0cccb852cc7
Axel RG <rg_axel@yahoo.fr>
2024-12-03 22:34:58 +0100

    relecture finale

--- BowlingGameKata/BowlingGame.cs
+++ BowlingGameKata/BowlingGame.cs
@@ -3,7 +3,6 @@
     public class BowlingGame
     {
         private readonly List<int[]> rolls = [];
-        public BowlingGame() { }
 
         public int AddRolls(IEnumerable<int[]> list)
         {
@@ -12,66 +11,72 @@
         }
         public int AddRoll(int[] roll)
         {
-            if (IsGameInProgress() || IsSpareGameBonus() || IsStrikeGameBonus())
-            {
-                int[] entry = roll.Length >= 2 ? roll.Take(2).ToArray() : [roll[0], 0];
-                rolls.Add(entry);
-            }
+            // checks constraints
+            var shouldAdd = IsGameInProgress() || IsSpareGameBonus() || IsStrikeGameBonus();
+            if (!shouldAdd) return rolls.Count;
+
+            // Act
+            int[] entry = roll.Length >= 2 ? roll.Take(2).ToArray() : [roll[0], 0];
+            rolls.Add(entry);
 
             return rolls.Count;
         }
+        #region AddRoll helpers
         private bool IsGameInProgress() => rolls.Count < 10;
-        private bool IsSpareGameBonus()
-        {
-            return rolls.Count == 10 && IsSpare(rolls[9]);
-        }
-        private bool IsStrikeGameBonus()
-        {
-            return 10 <= rolls.Count && rolls.Count < 12 && IsStrike(rolls[9]);
-        }
+        private bool IsSpareGameBonus() => rolls.Count == 10 && IsSpare(rolls[9]);
+        private bool IsStrikeGameBonus() => 10 <= rolls.Count && rolls.Count < 12 && IsStrike(rolls[9]);
+        #endregion
+
         public int GetScore()
         {
             int score = 0;
             for (int i = 0; i < rolls.Count; i++)
             {
+                // checks constraints
                 if (i >= 10) break;
+
+                // Act
                 int[] roll = rolls[i];
 
+                // Add the current roll's score
                 score += roll.Sum();
 
-                // Check for spare
-                score = ComputeSpare(score, i, roll);
-                score = ComputeStrike(score, i, roll);
+                // compute special scores
+                score = ComputeSpareScore(score, i, roll);
+                score = ComputeStrikeScore(score, i, roll);
             }
             return score;
         }
-
-        private int ComputeSpare(int score, int i, int[] roll)
+        private int ComputeSpareScore(int score, int i, int[] roll)
         {
-            if (IsSpare(roll) && i + 1 < rolls.Count)
-            {
-                score += rolls[i + 1][0]; // Add the next roll's first pin as bonus
-            }
+            // checks constraints
+            var shouldCompute = IsSpare(roll) && i + 1 < rolls.Count;
+            if (!shouldCompute) return score;
+
+            // Act
+            score += rolls[i + 1][0]; // Add the next roll's first pin as bonus            
 
             return score;
         }
-
-        private int ComputeStrike(int score, int i, int[] roll)
+        private int ComputeStrikeScore(int score, int i, int[] roll)
         {
-            if (IsStrike(roll) && i + 1 < rolls.Count)
-            {
-                score += rolls
-                    .Skip(i + 1)
-                    .SelectMany(roll => roll)
-                    .Where(roll => roll > 0)
-                    .Take(2)
-                    .Sum();
-            }
+            // checks constraints
+            var shouldCompute = IsStrike(roll) && i + 1 < rolls.Count;
+            if (!shouldCompute) return score;
+
+            // Act
+            score += rolls
+                .Skip(i + 1)
+                .SelectMany(roll => roll)
+                .Where(roll => roll > 0)
+                .Take(2)
+                .Sum();
 
             return score;
         }
-
+        #region GetScore helpers
         private bool IsSpare(int[] roll) => roll[0] < 10 && roll.Sum() == 10;
         private bool IsStrike(int[] roll) => roll[0] == 10;
+        #endregion
     }
 }
